12192e86f51ef128a9f01df0b2adc10a8ddd44ee
Rogelio Mitre <mitrerogelio@outlook.com>
2022-06-15 22:17:37 -0500

    rough form structure

--- contact.php
+++ contact.php
@@ -1,15 +1,14 @@
 <?php
 
 if (isset($POST['submit'])) {
-    $first_name = $_POST['first-name'];
-    $last_name = $_POST['last-name'];
+    $name = $_POST['first-name'];
     $mail_from = $_POST['email'];
-    $message = $_POST['subject'];
+    $message = $_POST['message'];
 
 
     $mail_to = "mitrerogelio@outlook.com";
     $headers = "From: " .$mail_from;
-    $txt = "You have received an email from " .$first_name.".\n\n".$message;
+    $txt = "You have received an email from " .$name.".\n\n".$message;
 
     mail($mail_to,$subject,$txt,$headers);
     header("Location: index.php?mailsend");
