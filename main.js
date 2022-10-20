c2890b9cbd23e39923a1d2e06172e1822a3f535a
Rogelio Mitre <mitrerogelio@outlook.com>
2022-10-20 09:18:05 -0500

    add animation, footer widths, js refactor (#26)

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const toggleButton = document.getElementById('toggle');
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-let menuOpen = false;
+const navbarLinks = document.getElementById('navbar-links');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('open');
-  if(!menuOpen) {
-    toggleButton.classList.add('flip');
-    menuOpen = true;
-  } else {
-    toggleButton.classList.remove('flip');
-    menuOpen = false;
-  }
+  toggleButton.classList.toggle('flip');
 })
