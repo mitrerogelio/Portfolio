7a706be7c71f6570b8b000c69eecf44a78e72956
Rogelio Mitre <mitrerogelio@outlook.com>
2022-10-19 21:27:06 -0500

    animate burger, add form

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const toggleButton = document.getElementsByClassName('toggle')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementById('toggle');
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+let menuOpen = false;
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('open')
+  navbarLinks.classList.toggle('open');
+  if(!menuOpen) {
+    toggleButton.classList.add('flip');
+    menuOpen = true;
+  } else {
+    toggleButton.classList.remove('flip');
+    menuOpen = false;
+  }
 })
